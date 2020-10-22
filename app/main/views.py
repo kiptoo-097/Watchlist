9c6654f13173e0d59b87fb978c7de3efdacff352
kiptoo-097 <kiptoo097.com>
2020-10-22 22:25:42 +0300

    adding unit test

--- app/main/views.py
+++ app/main/views.py
@@ -1,14 +1,12 @@
 from flask import render_template,request,redirect,url_for
-from app import app
-from .requests import get_movies,get_movie,search_movie
-from .models import review
+from . import main
+from ..requests import get_movies,get_movie,search_movie
 from .forms import ReviewForm
-Review = review.Review
-
+from ..models import Review
 
 
 # Views
-@app.route('/')
+@main.route('/')
 def index():
 
     '''
@@ -30,7 +28,7 @@ def index():
         return render_template('index.html', title = title, popular = popular_movies, upcoming = upcoming_movie, now_showing = now_showing_movie )
 
 
-@app.route('/movie/<int:id>')
+@main.route('/movie/<int:id>')
 def movie(id):
 
     '''
@@ -43,7 +41,8 @@ def movie(id):
     return render_template('movie.html',title = title,movie = movie,reviews = reviews)
 
 
-@app.route('/search/<movie_name>')
+
+@main.route('/search/<movie_name>')
 def search(movie_name):
     '''
     View function to display the search results
@@ -54,18 +53,22 @@ def search(movie_name):
     title = f'search results for {movie_name}'
     return render_template('search.html',movies = searched_movies)
 
-@app.route('/movie/review/new/<int:id>', methods = ['GET','POST'])
+
+@main.route('/movie/review/new/<int:id>', methods = ['GET','POST'])
 def new_review(id):
+
     form = ReviewForm()
+
     movie = get_movie(id)
 
     if form.validate_on_submit():
         title = form.title.data
         review = form.review.data
+
         new_review = Review(movie.id,title,movie.poster,review)
         new_review.save_review()
+
         return redirect(url_for('movie',id = movie.id ))
 
     title = f'{movie.title} review'
     return render_template('new_review.html',title = title, review_form=form, movie=movie)
-
---
+++ start.sh
@@ -0,0 +1,4 @@
+export MOVIE_API_KEY=9a8a5e47042bfe99a1fadd19966b8d6b
+export SECRET_KEY=Kiko
+
+python3.8 manage.py server
